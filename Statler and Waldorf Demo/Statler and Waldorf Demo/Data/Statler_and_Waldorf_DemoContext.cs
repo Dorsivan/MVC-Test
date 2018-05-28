9de816d367520f844dcd75e96fa67f6d79697c63
Dor Sivandddג <dorsiv@gmail.comd>
2018-05-28 21:32:49 +0300

    I hope this works

---
+++ Statler and Waldorf Demo/Statler and Waldorf Demo/Data/Statler_and_Waldorf_DemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Statler_and_Waldorf_Demo.Models
+{
+    public class Statler_and_Waldorf_DemoContext : DbContext
+    {
+        public Statler_and_Waldorf_DemoContext (DbContextOptions<Statler_and_Waldorf_DemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Statler_and_Waldorf_Demo.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Statler and Waldorf Demo/Statler and Waldorf Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Statler_and_Waldorf_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
